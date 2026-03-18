f6fb35f7ca39317fbaba730bc0882c6dd6cc56b3
Foster Clinton <foster626@gmail.com>
2026-03-18 00:13:23 -0700

    Remove bottom navigation, use hamburger menu only (#69)

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import MobileDrawer from "./MobileDrawer";
-import BottomNav from "./BottomNav";
 
 export default function Layout() {
   return (
@@ -11,11 +10,10 @@ export default function Layout() {
       <MobileDrawer />
       <div className="flex flex-1 flex-col overflow-hidden">
         <Header />
-        <main className="flex-1 overflow-y-auto p-4 pb-20 md:p-6 md:pb-6">
+        <main className="flex-1 overflow-y-auto p-4 md:p-6">
           <Outlet />
         </main>
       </div>
-      <BottomNav />
     </div>
   );
 }
